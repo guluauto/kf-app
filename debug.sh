59a64e431501719d273aaaec7ac993a5f6e67b55
Kane <dmccer@gmail.com>
2015-06-27 15:22:00 +0800

    rm www

---
+++ debug.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf ./www/*
+mv hooks/before_build hooks/before_build_bak
+cp -rf ../demigod/dist/* ./www
+grep -rl "o.dp:3000" ./www | xargs sed -i '' 's/o.dp:3000/t.ifdiu.com/g'
+cordova build android --debug
+mv hooks/before_build_bak hooks/before_build
+cordova run android --debug
+exit
