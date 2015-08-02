2e50cbba45a2f82fe3a168f9803a60bb5d849d74
Kane <dmccer@gmail.com>
2015-08-02 19:44:47 +0800

    pre

--- debug.sh
+++ debug.sh
@@ -5,7 +5,7 @@ mv hooks/before_build hooks/before_build_bak
 mv hooks/after_build hooks/after_build_bak
 
 cp -rf ../demigod/dist/* ./www
-grep -rl "o.dp:3000" ./www | xargs sed -i '' 's/o.dp:3000/cs.guluabc.com/g'
+grep -rl "o.dp:3001" ./www | xargs sed -i '' 's/o.dp:3001/cs.guluabc.com/g'
 cordova build android --debug
 
 mv hooks/before_build_bak hooks/before_build
