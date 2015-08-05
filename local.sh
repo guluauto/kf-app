6718378db6a3c65f811b47c680ed7381c7abaa01
Kane <dmccer@gmail.com>
2015-08-05 22:41:57 +0800

    del port

--- local.sh
+++ local.sh
@@ -5,7 +5,7 @@ mv hooks/before_build hooks/before_build_bak
 mv hooks/after_build hooks/after_build_bak
 
 cp -rf ../demigod/dist/* ./www
-grep -rl "o.dp:3000" ./www | xargs sed -i '' 's/o.dp:3000/192.168.31.34:8000/g'
+grep -rl "o.dp:3000" ./www | xargs sed -i '' 's/o.dp:3000/192.168.31.34/g'
 cordova build android --debug
 
 mv hooks/before_build_bak hooks/before_build
