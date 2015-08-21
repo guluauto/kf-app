89c3d189ee98d6438b3420a3575685c05f0076e0
Kane <dmccer@gmail.com>
2015-08-21 10:17:31 +0800

    deploy sh

--- publish.sh
+++ publish.sh
@@ -5,12 +5,12 @@ mv hooks/before_build hooks/before_build_bak
 mv hooks/after_build hooks/after_build_bak
 
 cp -rf ../demigod/dist/* ./www
-grep -rl "o.dp:3000" ./www | xargs sed -i '' 's/o.dp:3000/http://gulu.0yuchen.com/g'
-cordova build android --debug
+grep -rl "o.dp:3001" ./www | xargs sed -i '' 's/o.dp:3001/cs.guluauto.com/g'
+cordova build android --release
 
 mv hooks/before_build_bak hooks/before_build
 mv hooks/after_build_bak hooks/after_build
 
 # cordova run android --debug
-scp ./platforms/android/ant-build/MainActivity-debug.apk xyh@guluabc.com:/home/xyh/app-getter/public/kf-local.apk
+scp ./platforms/android/ant-build/MainActivity-release.apk xyh@guluabc.com:/home/xyh/app-getter/public/kf-prd.apk
 exit
